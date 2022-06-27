8caca1cf9c943240fe479f5388508fae0044a579
Labaile <Labaile1@gmail.com>
2022-06-27 19:20:31 -0400

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
-    }
-}
+        Car car = new Car();
+        car.go();
+        System.out.println("This car has " + car.doors + " doors");
+        
+        Bicycle bike = new Bicycle();
+        bike.stop();
+        System.out.println("This bike has " + bike.wheels + " wheels");
+
+    }//close main
+}//close class
